34f214f89c8111af2635ec1cacb237650bb9be18
oaazk <164911941+oaazk@users.noreply.github.com>
2024-05-06 14:38:35 +0400

    Add files via upload

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var cash;
     var lemons;
     var lemonade;
     var timer;
-    var timeLimit = 60000; // 1 minutes
+    var timeLimit = 60000; // 2 minutes
     var gameOver = false;
 
     function startGame() {
@@ -42,9 +42,9 @@ var cash;
     function buyLemons() {
       if (!gameOver) {
         if (cash >= 1) {
-          // Buy a lemon and update the cash and lemon count
-          cash -= 2;
-          lemons += 3;
+          // Buy a lemon and update the budget and lemon count
+          cash -= 1;
+          lemons += 1;
           document.getElementById("cash").innerHTML = "Cash: $" + cash;
           document.getElementById("lemons").innerHTML = "Lemons: " + lemons;
         } else {
@@ -62,7 +62,7 @@ var cash;
           lemonade += 1;
           document.getElementById("lemons").innerHTML = "Lemons: " + lemons;
           document.getElementById("lemonade").innerHTML = "Lemonade: " + lemonade;
-          setTimeout(lemonade--, 6000); // Set a timer to "spoil" the lemonade after 6 seconds
+          setTimeout(sellLemonade, 8000); // Set a timer to sell the lemonade after 8 seconds
         } else {
           // Show an alert if there are not enough lemons to make lemonade
           alert("Not enough lemons to make lemonade!");
@@ -75,7 +75,7 @@ var cash;
         if (lemonade >= 1) {
           // Sell a lemonade, update the budget and lemonade count
           lemonade -= 1;
-          cash += 4;
+          cash += 5;
           document.getElementById("cash").innerHTML = "Cash: $" + cash;
           document.getElementById("lemonade").innerHTML = "Lemonade: " + lemonade;
         }
@@ -92,9 +92,9 @@ var cash;
 
       if (won) {
         // Display a congratulatory message if the player wins
-        document.getElementById("result").innerHTML = "Congratulations! You raised $160 for charity!";
+        document.getElementById("result").innerHTML = "Congratulations! You raised $150 for charity!";
       } else {
         // Display a game over message if the player loses
-        document.getElementById("result").innerHTML = "Game over! You didn't reach $160.";
+        document.getElementById("result").innerHTML = "Game over! You didn't reach double your budget.";
       }
     }
